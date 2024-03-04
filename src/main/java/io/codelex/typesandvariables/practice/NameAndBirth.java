a358fd53855e6d6291e4776da15a6a300a227414
ShadyWestHaze <zakatistovu@gmail.com>
2024-03-04 12:05:05 +0200

    Name and birth year

---
+++ src/main/java/io/codelex/typesandvariables/practice/NameAndBirth.java
@@ -0,0 +1,13 @@
+package src.main.java.io.codelex.typesandvariables.practice;
+
+import java.util.Date;
+
+public class NameAndBirth {
+    public static void main(String[] args){
+
+        String name = "Krisjanis";
+        int birthYear = 2002;
+
+        System.out.printf("My name is %s and I was born in %d.", name, birthYear);
+    }
+}
