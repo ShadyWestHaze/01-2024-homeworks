7d22d1c5186e8c70f558f4db5007193abfcd42a3
ShadyWestHaze <zakatistovu@gmail.com>
2024-03-04 11:58:54 +0200

    Sum Average

--- src/main/java/io/codelex/arithmetic/practice/SumAverageRunningInt.java
+++ src/main/java/io/codelex/arithmetic/practice/SumAverageRunningInt.java
@@ -1,14 +1,22 @@
 package io.codelex.arithmetic.practice;
 
+// text print formating format() info https://www.developer.com/java/java-string-format-method/
 public class SumAverageRunningInt {
     public static void main(String[] args) {
         int sum = 0;
-        int average;
         int lowerBound = 1;
         int upperBound = 100;
+        double average;
 
         for (int number = lowerBound; number <= upperBound; ++number) {
             sum += number;
         }
+
+        average = (double ) sum / upperBound;
+
+        //printf() = format()
+        System.out.printf("The sum of %d to %d is %d%n", lowerBound, upperBound, sum);
+        System.out.printf("The average is %.1f%n ", average);
+
     }
 }
