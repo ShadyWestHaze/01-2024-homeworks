33fce52fff78beb3e7e13c46f89fb314d1f116d1
ShadyWestHaze <zakatistovu@gmail.com>
2024-03-04 18:19:52 +0200

    Sad for not sad anymore

--- src/main/java/io/codelex/typesandvariables/practice/SadFor.java
+++ src/main/java/io/codelex/typesandvariables/practice/SadFor.java
@@ -1,19 +1,25 @@
 package io.codelex.typesandvariables.practice;
-
 import io.codelex.NoCodeWrittenException;
 
 public class SadFor {
     public static void main(String[] args) {
-        System.out.println(test1());
+        try {
+            System.out.println(test1());
+        } catch (NoCodeWrittenException e) {
+            System.out.println("No number has a square root higher then 2.5");
+        }
     }
 
     public static int test1() {
-//fixme
-//        for (int i = 0; i < 10; i++) {
-//            if (Math.sqrt(i) > 2.5)
-//                break;
-//        }
-//        return i;
-        throw new NoCodeWrittenException();
+        int i ;
+        int j =6;
+       for (i=0; i <= j; i++) {
+           if (Math.sqrt(i) > 2.5){
+               break;
+           }
+        }
+       if(i==j){throw new NoCodeWrittenException();}
+        return i;
+
     }
 }
