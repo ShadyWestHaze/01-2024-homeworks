1f8bd80e72656abac31cab41d8baf54f3b207d59
ShadyWestHaze <zakatistovu@gmail.com>
2024-03-05 15:06:21 +0200

    Calculate area / geometry Done

--- src/main/java/io/codelex/arithmetic/practice/Geometry.java
+++ src/main/java/io/codelex/arithmetic/practice/Geometry.java
@@ -1,19 +1,29 @@
 package io.codelex.arithmetic.practice;
 
-import io.codelex.NoCodeWrittenException;
-
 import java.math.BigDecimal;
 
-class Geometry {
-    static double areaOfCircle(BigDecimal radius) {
-        throw new NoCodeWrittenException();
+class Geometry {                                                //Area = π × r2
+    static BigDecimal areaOfCircle(BigDecimal radius) {
+        if (radius.signum() <= 0) {
+            throw new ArithmeticException("Radius of circle has to be positive");
+        }
+        BigDecimal pi = new BigDecimal(Math.PI);
+        return pi.multiply(radius).multiply(radius);
     }
 
-    static double areaOfRectangle(BigDecimal length, BigDecimal width) {
-        throw new NoCodeWrittenException();
+    static BigDecimal areaOfRectangle(BigDecimal length, BigDecimal width) {
+        if (length.signum() <= 0 ||width.signum() <= 0) {
+            throw new ArithmeticException("Size of length and width have to be positive");
+        }
+        return length.multiply(width);
     }
 
-    static double areaOfTriangle(BigDecimal base, BigDecimal h) {
-        throw new NoCodeWrittenException();
+    static BigDecimal areaOfTriangle(BigDecimal base, BigDecimal h) {
+        if (base.signum() <= 0 ||h.signum() <= 0 ) {
+            throw new ArithmeticException("Size of base and height have to be positive");
+        }
+
+
+        return base.multiply(h).divide(BigDecimal.valueOf(2));
     }
 }
