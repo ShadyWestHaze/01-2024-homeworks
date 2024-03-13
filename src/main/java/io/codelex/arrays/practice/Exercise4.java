0bb77594391f603f8f37907fa243fd748c7b62c4
ShadyWestHaze <zakatistovu@gmail.com>
2024-03-13 12:00:54 +0200

    Arrays 5

--- src/main/java/io/codelex/arrays/practice/Exercise4.java
+++ src/main/java/io/codelex/arrays/practice/Exercise4.java
@@ -9,13 +9,14 @@ public class Exercise4 {
                 1458, 2458, 1254, 1472, 2365,
                 1456, 2265, 1457, 2456
         };
+        int valueToLookFor = 1254;
 
-        /*
-        fixme - check if contains number 1245 
-        for (?) {
-            ......
-            System.out.println("Contains!");
+        for (int nums : myArray) {
+            if (nums == valueToLookFor) {
+                System.out.println("Contains!");
+            }
+
         }
-        */
+
     }
 }
