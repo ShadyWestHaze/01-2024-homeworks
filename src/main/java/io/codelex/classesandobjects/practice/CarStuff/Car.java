579163d06bbdad6921c1f1697d55df188387a908
ShadyWestHaze <zakatistovu@gmail.com>
2024-04-03 13:15:46 +0300

    Uzliku access modifiers. Nezinu kā veselam file nebiju nevienu uzlicis hah

--- src/main/java/io/codelex/classesandobjects/practice/CarStuff/Car.java
+++ src/main/java/io/codelex/classesandobjects/practice/CarStuff/Car.java
@@ -1,32 +1,32 @@
 package src.main.java.io.codelex.classesandobjects.practice.CarStuff;
 
-class Car {
+public class Car {
     private final double startKilometers;
     private double endKilometers;
     private double totalLiters;
 
-    Car(double startKilometers) {
+    public Car(double startKilometers) {
         this.startKilometers = startKilometers;
 
     }
 
-    void fillUp(int mileage, double liters) {
+    public void fillUp(int mileage, double liters) {
         this.endKilometers = mileage;
         this.totalLiters += liters;
     }
 
-    double calculateConsumption() {
+    public double calculateConsumption() {
         if (startKilometers == endKilometers) {
             return 0.0;
         }
         return (totalLiters / (endKilometers - startKilometers)) * 100;
     }
 
-    boolean gasHog() {
+    public boolean gasHog() {
         return calculateConsumption() > 15.0;
     }
 
-    boolean economyCar() {
+    public boolean economyCar() {
         return calculateConsumption() < 5.0;
     }
 
