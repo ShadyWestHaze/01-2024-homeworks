9f7dd12b43cd5e414ed747b1f0155594a2d7c1d7
ShadyWestHaze <zakatistovu@gmail.com>
2024-04-03 14:04:20 +0300

    Noņēmu caps.

--- src/main/java/io/codelex/dateandtime/practice/DatePeriod.java
+++ src/main/java/io/codelex/dateandtime/practice/DatePeriod.java
@@ -3,25 +3,25 @@ package src.main.java.io.codelex.dateandtime.practice;
 import java.time.LocalDate;
 
 public class DatePeriod {
-    private final LocalDate START;
-    private final LocalDate END;
+    private final LocalDate start;
+    private final LocalDate end;
 
     public DatePeriod(LocalDate start, LocalDate end) {
-        this.START = start;
-        this.END = end;
+        this.start = start;
+        this.end = end;
     }
 
     public LocalDate getStart() {
-        return START;
+        return start;
     }
 
     public LocalDate getEnd() {
-        return END;
+        return end;
     }
 
     public DatePeriod intersection(DatePeriod other) {
-        LocalDate intersectionStart = this.START.isAfter(other.START) ? this.START : other.START;
-        LocalDate intersectionEnd = this.END.isBefore(other.END) ? this.END : other.END;
+        LocalDate intersectionStart = this.start.isAfter(other.start) ? this.start : other.start;
+        LocalDate intersectionEnd = this.end.isBefore(other.end) ? this.end : other.end;
         if (intersectionStart.isAfter(intersectionEnd)) {
             return null;
         } else {
