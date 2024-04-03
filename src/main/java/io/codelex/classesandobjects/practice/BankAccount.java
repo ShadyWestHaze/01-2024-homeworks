79c7f37f16b162072b96e8532a6b131193635d74
ShadyWestHaze <zakatistovu@gmail.com>
2024-04-03 13:14:10 +0300

    Bank account main klase uz test file

--- src/main/java/io/codelex/classesandobjects/practice/BankAccount.java
+++ src/main/java/io/codelex/classesandobjects/practice/BankAccount.java
@@ -1,38 +1,11 @@
 package io.codelex.classesandobjects.practice;
 
-import java.util.Scanner;
 
 // Each BankAccount object represents one user's account
 // information including name and balance of money.
 public class BankAccount {
     public String name;
     public double balance;
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        BankAccount account = new BankAccount();
-
-        System.out.print("Enter account name: ");
-        account.name = scanner.nextLine();
-
-        System.out.print("Enter initial balance: $");
-        account.balance = scanner.nextDouble();
-
-        System.out.println("Account created: " + account);
-
-        System.out.print("Enter deposit amount: $");
-        double depositAmount = scanner.nextDouble();
-        account.deposit(depositAmount);
-
-        System.out.println("Current balance after deposit: " + account);
-
-        System.out.print("Enter withdrawal amount: $");
-        double withdrawAmount = scanner.nextDouble();
-        account.withdraw(withdrawAmount);
-
-        System.out.println("Current balance after withdrawal: " + account);
-
-    }
 
     public void deposit(double amount) {
         if (amount > 0) {
@@ -61,4 +34,5 @@ public class BankAccount {
         }
     }
 }
+
 
