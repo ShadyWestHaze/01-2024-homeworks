dc07cca4af5da40952f6d30be8df74a17079ac29
ShadyWestHaze <zakatistovu@gmail.com>
2024-04-09 11:29:34 +0300

    Exercise 4 missing one.

--- src/main/java/io/codelex/polymorphism/practice/exercise4/StaffMember.java
+++ src/main/java/io/codelex/polymorphism/practice/exercise4/StaffMember.java
@@ -1,4 +1,4 @@
-package io.codelex.polymorphism.practice.exercise4;
+package src.main.java.io.codelex.polymorphism.practice.exercise4;
 
 abstract public class StaffMember {
     protected String name;
