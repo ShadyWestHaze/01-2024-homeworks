67492a5e9aa938c0e44f5e798127954848bf253b
ShadyWestHaze <zakatistovu@gmail.com>
2024-04-17 20:59:19 +0300

    Exercise1

---
+++ src/main/java/io/codelex/JavaAdvancedTest/BankCards/BankCard.java
@@ -0,0 +1,11 @@
+package io.codelex.JavaAdvancedTest.BankCards;
+
+public interface BankCard {
+
+    String fullName();
+
+    double addMoney(double money);
+
+    double takeMoney(double money);
+
+}
